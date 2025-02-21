48a6af86f2c2ae3e0a052be5c7c933aa0cdd4ff2
Blue <chuduong2504@gmail.com>
2025-02-21 18:16:27 +0700

    Initial commit

---
+++ ThucHanhWebAPInetcoreEntity/Controllers/StudentsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ThucHanhWebAPInetcoreEntity.Models;
+using System;
+using ThucHanhWebAPInetcoreEntity.Data; // Chứa StudentContext
+
+namespace ThucHanhWebAPInetcoreEntity.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly StudentContext _context;
+
+        public StudentsController(StudentContext context)
+        {
+            _context = context;
+        }
+
+        //action
+        [HttpPost("CreateStudent")]
+        public async Task<IActionResult> CreateStudent(Student student)
+        {
+
+            _context.Students.Add(student);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { message = "Student added successfully", student });
+
+        }
+
+    }
+}
---
+++ ThucHanhWebAPInetcoreEntity/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ThucHanhWebAPInetcoreEntity.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+if (1 < 0) // gán cứng bằng lệnh
+    builder.Services.AddDbContext<StudentContext>(options =>
+                options.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=CMCUni2;Integrated Security=True;"));
+else // lấy từ cấu hình appsettings.json
+    builder.Services.AddDbContext<StudentContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("StudentDBConnectionString")));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
